91b7f3e67f9c776f9a23221bf70460803b7826a6
ze <3101135386@qq.com>
2016-02-22 23:14:34 +0800

    first

---
+++ MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStore.Models;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>
+          {
+              new Genre{Name="Disco"},
+              new Genre{Name="Jazz"},
+              new Genre{Name="Rock"}
+          };
+            return View(genres);
+        }
+
+        //http://localhost:2586/Store/Browse?genre=here
+        /*public string Browse(string genre)
+        {
+           string message = HttpUtility.HtmlEncode("Store.Browse Genre=" + genre);
+            return message;
+        }*/
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+        public ActionResult test()
+        {
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album" + id };
+            return View(album);
+        }
+    }
+}
---
+++ MVCMusicStore/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+    //唱片类
+    public class Genre
+    {
+        public string Name { get; set; }
+    }
+}
